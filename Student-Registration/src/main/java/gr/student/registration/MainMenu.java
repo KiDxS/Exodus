8fb196cfd3e5bd4d9116ea5aebde79b1dd59912a
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-05-27 14:08:26 +0800

    Renamed the variables

--- Student-Registration/src/main/java/gr/student/registration/MainMenu.java
+++ Student-Registration/src/main/java/gr/student/registration/MainMenu.java
@@ -30,11 +30,11 @@ public class MainMenu extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        tableScrollPane = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        editStudentButton = new javax.swing.JButton();
+        addStudentButton = new javax.swing.JButton();
+        deleteStudentButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setFocusCycleRoot(false);
@@ -49,9 +49,9 @@ public class MainMenu extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("MAIN MENU");
 
-        jScrollPane1.setBackground(new java.awt.Color(204, 255, 204));
-        jScrollPane1.setFont(new java.awt.Font("sansserif", 0, 18));
-        jScrollPane1.getVerticalScrollBar().setBackground(new java.awt.Color(204, 255, 204));
+        tableScrollPane.setBackground(new java.awt.Color(204, 255, 204));
+        tableScrollPane.setFont(new java.awt.Font("sansserif", 0, 18));
+        tableScrollPane.getVerticalScrollBar().setBackground(new java.awt.Color(204, 255, 204));
 
         jTable1.setAutoCreateRowSorter(true);
         jTable1.setBackground(new java.awt.Color(204, 204, 204));
@@ -63,7 +63,7 @@ public class MainMenu extends javax.swing.JFrame {
                 {"Cute ko",  new Integer(2)}
             },
             new String [] {
-                "Full name", "School Id"
+                "Full name", "Student ID"
             }
         ) {
             Class[] types = new Class [] {
@@ -86,35 +86,35 @@ public class MainMenu extends javax.swing.JFrame {
         jTable1.setSelectionBackground(new java.awt.Color(255, 255, 255));
         jTable1.setSelectionForeground(new java.awt.Color(0, 0, 0));
         jTable1.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        jScrollPane1.setViewportView(jTable1);
+        tableScrollPane.setViewportView(jTable1);
 
-        jButton1.setBackground(new java.awt.Color(29, 185, 196));
-        jButton1.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Edit");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        editStudentButton.setBackground(new java.awt.Color(29, 185, 196));
+        editStudentButton.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        editStudentButton.setForeground(new java.awt.Color(255, 255, 255));
+        editStudentButton.setText("Edit");
+        editStudentButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                editStudentButtonActionPerformed(evt);
             }
         });
 
-        jButton2.setBackground(new java.awt.Color(0, 128, 55));
-        jButton2.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("+");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        addStudentButton.setBackground(new java.awt.Color(0, 128, 55));
+        addStudentButton.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        addStudentButton.setForeground(new java.awt.Color(255, 255, 255));
+        addStudentButton.setText("+");
+        addStudentButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                addStudentButtonActionPerformed(evt);
             }
         });
 
-        jButton3.setBackground(new java.awt.Color(196, 29, 29));
-        jButton3.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("Delete");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        deleteStudentButton.setBackground(new java.awt.Color(196, 29, 29));
+        deleteStudentButton.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        deleteStudentButton.setForeground(new java.awt.Color(255, 255, 255));
+        deleteStudentButton.setText("Delete");
+        deleteStudentButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                deleteStudentButtonActionPerformed(evt);
             }
         });
 
@@ -126,16 +126,16 @@ public class MainMenu extends javax.swing.JFrame {
                 .addContainerGap(355, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton3)
+                        .addComponent(deleteStudentButton)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton1)
+                        .addComponent(editStudentButton)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton2)
+                        .addComponent(addStudentButton)
                         .addGap(46, 46, 46))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(350, 350, 350))))
-            .addComponent(jScrollPane1)
+            .addComponent(tableScrollPane)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -143,12 +143,12 @@ public class MainMenu extends javax.swing.JFrame {
                 .addGap(12, 12, 12)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 525, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(tableScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 525, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(26, 26, 26)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addComponent(addStudentButton, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(editStudentButton, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(deleteStudentButton, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addContainerGap(22, Short.MAX_VALUE))
         );
 
@@ -167,18 +167,18 @@ public class MainMenu extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void editStudentButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editStudentButtonActionPerformed
         // TODO add your handling code here:is
+    }//GEN-LAST:event_editStudentButtonActionPerformed
+
+    private void addStudentButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addStudentButtonActionPerformed
+        // TODO add your handling code here:
         addStudentPanel.setVisible(true);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_addStudentButtonActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void deleteStudentButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteStudentButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_deleteStudentButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -217,12 +217,12 @@ public class MainMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton addStudentButton;
+    private javax.swing.JButton deleteStudentButton;
+    private javax.swing.JButton editStudentButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JScrollPane tableScrollPane;
     // End of variables declaration//GEN-END:variables
 }
