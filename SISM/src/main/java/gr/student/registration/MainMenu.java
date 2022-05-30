4a243a6fb85eb75a7e82b0f86522c0631a1e0cc3
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-05-30 17:15:07 +0800

    Renamed StudentPanel to addStudentForm

--- SISM/src/main/java/gr/student/registration/MainMenu.java
+++ SISM/src/main/java/gr/student/registration/MainMenu.java
@@ -18,7 +18,7 @@ public class MainMenu extends javax.swing.JFrame {
         initComponents();
     }
 
-    StudentPanel addStudentPanel = new StudentPanel();
+    addStudentForm addStudentPanel = new addStudentForm();
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
