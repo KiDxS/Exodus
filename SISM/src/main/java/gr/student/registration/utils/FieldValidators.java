31df0287fd9f5e320a02881d1e053ba8263e4b8d
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-06-03 14:48:15 +0800

    Added validators for the fields to validate data.

--- SISM/src/main/java/gr/student/registration/utils/FieldValidators.java
+++ SISM/src/main/java/gr/student/registration/utils/FieldValidators.java
@@ -2,9 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package gr.student.registration.utils;
 
+import java.util.Map;
+import javax.swing.JOptionPane;
+
 public class FieldValidators {
+    public int Validate(Map<String, String> dictionary) {
+        int statusCode; // returns 0 if there's no error; returns 1 if there's an error
+        
+        // Iterates over the keys
+        for (String key : dictionary.keySet()) {
+            String value = dictionary.get(key); // Returns the value assigned to the key
+            if (value.isEmpty()) {
+                JOptionPane.showMessageDialog(null, String.format("%s is required", key), "Field value is missing", JOptionPane.WARNING_MESSAGE);
+                return statusCode = 1;
+            }
+            
+            // If the key is LRN
+            if ("LRN".equals(key)) {
+                
+                // Checks the length of the LRN
+                if (value.length() != 12) {
+                    JOptionPane.showMessageDialog(null, String.format("%s requires a length of 12", key), "LRN Length", JOptionPane.WARNING_MESSAGE);
+                    return statusCode = 1;
+                }
+
+            }
+        }
+        return statusCode = 0;
+
+    }
 
 }
