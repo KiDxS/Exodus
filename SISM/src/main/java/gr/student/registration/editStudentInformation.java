8633fa03f902f2f635cefba67a8385b53c035cc6
Angelo Geant Gaviola <gaviola.angelogeant@dnsc.edu.ph>
2022-06-06 13:27:51 +0800

    Update editStudentInformation.java
    Added a empty validator for the two fields.

--- SISM/src/main/java/gr/student/registration/editStudentInformation.java
+++ SISM/src/main/java/gr/student/registration/editStudentInformation.java
@@ -489,6 +489,8 @@ public class editStudentInformation extends javax.swing.JDialog {
             dictionary.put("Contact Number", contactNumber);
             dictionary.put("City", city);
             dictionary.put("Address", address);
+            dictionary.put("Middle Name", middleName);
+            dictionary.put("Suffix", suffix);
             FieldValidators validator = new FieldValidators();
 
             // If the value returned is not 1 then continue
